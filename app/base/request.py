1e584743ebb4b27cf9f79f098a495f127a4398a3
Juseq <ilamikia295@gmail.com>
2025-11-25 22:35:31 +0500

    Update request.py

--- app/base/request.py
+++ app/base/request.py
@@ -5,10 +5,10 @@ from sqlalchemy import select
 # Writing a user id to the database
 async def set_user(tg_id: int) -> None:
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        user = await session.scalar(select(Users).where(Users.tg_id == tg_id))
         
         #If the user is not in the database, then he will write it down.
         #This database was created for fun, it can be removed.
         if not user:
-            session.add(User(tg_id=tg_id))
+            session.add(Users(tg_id=tg_id))
             await session.commit()
